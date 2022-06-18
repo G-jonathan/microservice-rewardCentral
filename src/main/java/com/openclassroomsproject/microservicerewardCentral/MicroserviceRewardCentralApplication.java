716a8507c24a8cbe29ab6eb424a444f186ffe70d
jonathan <jonathan.developpeur@gmail.com>
2022-06-18 10:26:08 +0200

    create_github_repo

---
+++ src/main/java/com/openclassroomsproject/microservicerewardCentral/MicroserviceRewardCentralApplication.java
@@ -0,0 +1,12 @@
+package com.openclassroomsproject.microservicerewardCentral;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceRewardCentralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceRewardCentralApplication.class, args);
+	}
+}
---
+++ src/test/java/com/openclassroomsproject/microservicerewardCentral/MicroserviceRewardCentralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openclassroomsproject.microservicerewardCentral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceRewardCentralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
